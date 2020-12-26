25ea56ba46e4e7ac250a1d1ea8903871dccbf0ff
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2020-12-26 19:31:53 +0100

    tested code

---
+++ FinalAssignment/rps_try/README.md
@@ -0,0 +1,14 @@
+
+Used this Starter code: Broken Rock Paper Scissors
+---------------------------------------------------
+
+http://www.wa4e.com/code/rps/
+
+
+To replicate the working version of this application:
+------------------------------------------------------
+http://www.wa4e.com/solutions/rps/
+
+
+
+
---
+++ FinalAssignment/rps_try/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Linda Angulo Lopez - 0ea50e88</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Rock Paper Scissors</h1>
+<a href="login.php">Please Log In</a>
+<p>Attempt to go to <a href="game.php">game.php</a> without loggin in - it should fail with an error message.</p>	
+</div>
+</body>
